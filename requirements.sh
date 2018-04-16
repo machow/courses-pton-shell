bcb310d5262941b973e71d0aab661a2db490bc02
Michael Chow <machow@princeton.edu>
2018-04-16 13:04:52 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,13 @@
 # If bash command fails, build should error out
 set -e
 
-##### Install specific package versions with pip #####
+cd /home/repl
 
-# pip3 install pandas==0.22.0
-# pip3 install matplotlib==2.1.2
-# pip3 install scikit-learn==0.19.1
+apt-get update
+apt-get install -y unzip
 
+curl https://swcarpentry.github.io/shell-novice/data/data-shell.zip -O data-shell.zip
+unzip data-shell.zip
+rm data-shell.zip
 
+rsync -a /home/repl/ /.course_home/
